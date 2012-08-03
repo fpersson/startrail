224ab0d1abc79e9682be223dd5fb2acf05a0fc9c
Fredrik Persson <fpersson.se@gmail.com>
2012-08-03 14:44:32 +0200

    bygger med boost_unittest

--- src/config.h
+++ src/config.h
@@ -1,6 +1,6 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-const std::string software_version("2.2.0");
+const std::string software_version("2.2.1");
 
 #endif 
--- test/unittest.cpp
+++ test/unittest.cpp
@@ -1,38 +1,40 @@
-#include <gtest/gtest.h>
+#define BOOST_TEST_MAIN
 #include <vector>
 #include <string>
 #include <stdio.h>
+#include <boost/test/included/unit_test.hpp>
 #include "../src/CCfgReader.h"
 #include "../src/CProjReader.h"
 #include "../src/CProjWriter.h"
 
-TEST(ConfigReaderTest, inittest){
-  CConfigRead reader("./test_data/testfile1.txt");
-  std::vector<std::string> res = reader.GetFiles();
-  EXPECT_EQ("./test_data/f1.jpg", res[0]);
-  EXPECT_EQ("./test_data/f2.jpg", res[1]);
-  EXPECT_EQ("./test_data/f3.jpg", res[2]);
+BOOST_AUTO_TEST_CASE(ConfigReaderTest_inittest){
+    CConfigRead reader("./test_data/testfile1.txt");
+    std::vector<std::string> res = reader.GetFiles();
+
+    BOOST_CHECK("./test_data/f1.jpg" == res[0]);
+    BOOST_CHECK("./test_data/f2.jpg" == res[1]);
+    BOOST_CHECK("./test_data/f3.jpg" == res[2]);
 }
 
-TEST(ProjectReader, init){
+BOOST_AUTO_TEST_CASE(ProjectReader_init){
   CProjReader reader("./test_data/testfile1.proj");
   int api_ver = reader.getApiVersion();
   std::string destfile = reader.getDestFile();
   
-  EXPECT_EQ(1, api_ver);
-  EXPECT_EQ("./dir/test.jpg", destfile);
+  BOOST_CHECK(1 == api_ver);
+  BOOST_CHECK("./dir/test.jpg" == destfile);    
 }
 
-TEST(ProjectReader, files){
+BOOST_AUTO_TEST_CASE(ProjectReader_files){
   CProjReader reader("./test_data/testfile1.proj");
   std::vector<std::string> res = reader.GetFiles();
   
-  EXPECT_EQ("./test_data/f1.jpg", res[0]);
-  EXPECT_EQ("./test_data/f2.jpg", res[1]);
-  EXPECT_EQ("./test_data/f3.jpg", res[2]);
+  BOOST_CHECK("./test_data/f1.jpg" == res[0]);
+  BOOST_CHECK("./test_data/f2.jpg" == res[1]);
+  BOOST_CHECK("./test_data/f3.jpg" == res[2]);    
 }
 
-TEST(ProjectReader, WriteRead){
+BOOST_AUTO_TEST_CASE(ProjectReader_WriteRead){
   std::string testfile = "./test_data/foo.proj";
   std::vector<std::string> files;
   
@@ -48,21 +50,14 @@ TEST(ProjectReader, WriteRead){
   std::string destfile = reader.getDestFile();
   std::vector<std::string> res = reader.GetFiles();
   
-  EXPECT_EQ(1, api_ver);
-  EXPECT_EQ("./dir/test.jpg", destfile);
+  BOOST_CHECK(1 == api_ver);
+  BOOST_CHECK("./dir/test.jpg" == destfile);
   
-  EXPECT_EQ("./test_data/f1.jpg", res[0]);
-  EXPECT_EQ("./test_data/f2.jpg", res[1]);
-  EXPECT_EQ("./test_data/f3.jpg", res[2]);
+  BOOST_CHECK("./test_data/f1.jpg" == res[0]);
+  BOOST_CHECK("./test_data/f2.jpg" == res[1]);
+  BOOST_CHECK("./test_data/f3.jpg" == res[2]);
   
   if( remove(testfile.c_str()) != 0){
     std::cout << "Error: could not delete " << testfile << std::endl;
   }
-}
-
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
-    return 0;
 }
